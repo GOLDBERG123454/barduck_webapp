06a2e0944a67847b54cc74dcc8f025f440f80b19
GOLDBERG123454 <116569951+GOLDBERG123454@users.noreply.github.com>
2025-05-15 01:58:05 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 window.Telegram.WebApp.ready();
 const tg = window.Telegram.WebApp;
 const tgId = tg.initDataUnsafe.user?.id;
-document.getElementById('user').innerText = `Привет, ${tg.initDataUnsafe.user?.first_name || 'Утка'}!`;
+document.getElementById("user").innerText = `Привет, ${tg.initDataUnsafe.user?.first_name || 'Утка'}!`;
 
 function sendAction(action) {
     tg.sendData(action);
 }
 
-// Загружаем данные игрока с backend API
-fetch(`http://127.0.0.1:5000/api/user/${tgId}`)
+fetch("https://e57f-51-158-165-38.ngrok-free.app/api/user/" + tgId)
     .then(res => res.json())
     .then(data => {
         if (data.error) {
             alert("Ошибка: " + data.error);
             return;
         }
-        document.getElementById('brd').innerText = data.brd;
-        document.getElementById('hp').innerText = data.hp;
-        document.getElementById('atk').innerText = data.atk;
-        document.getElementById('xp').innerText = data.xp;
-        document.getElementById('lvl').innerText = data.lvl;
+        document.getElementById("brd").innerText = data.brd;
+        document.getElementById("hp").innerText = data.hp;
+        document.getElementById("atk").innerText = data.atk;
+        document.getElementById("xp").innerText = data.xp;
+        document.getElementById("lvl").innerText = data.lvl;
     })
     .catch(err => {
         console.error("API error:", err);
